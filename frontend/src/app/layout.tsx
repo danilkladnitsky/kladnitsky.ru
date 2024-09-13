636add5eef43528d915ad6fc892780387ce904b3
danilkladnitsky <danyakladnitsky@gmail.com>
2024-09-13 14:46:04 +0300

    feat: add yandex webmaster verification

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
     return (
         <html lang="ru">
             <Head>
+                <meta name="yandex-verification" content="e8d8f768342d4d46" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
